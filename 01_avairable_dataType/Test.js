5a722fd148f48e4b956260f79e6b140e2c9004f8
Aigest <aigest@gmail.com>
2023-05-31 21:04:24 -0400

    Class code

---
+++ 01_avairable_dataType/Test.js
@@ -0,0 +1,13 @@
+
+
+myName = "Aigest"
+const myName = 'Aigest'; 
+console.log(myName);
+
+
+let myAge = 30;
+console.log (typeof myAge);
+
+
+let isStudent = true;
+console.log (isStudent);
---
+++ 01_avairable_dataType/variable.js
@@ -0,0 +1,12 @@
+let age = 27;
+let isDoorClosed = false;
+let fullName = 'John Doe';
+let time = null;
+
+let person = {
+    firstName : 'John',
+    lastName : 'Doe',
+    age : 27
+}
+
+console.log(typeof person);
