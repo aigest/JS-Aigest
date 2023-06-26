0952238f6bee39a1bfe841561221f627cab3a154
aigest <134978327+aigest@users.noreply.github.com>
2023-06-26 18:06:15 -0400

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,45 @@
+let num1=10, num2=20;
+console.log(num1+num2);
+
+
+let age1=20;
+let age2=30;
+console.log(`\n Is 1 older than 2 ? ${age1>age2}`);
+
+let name1='Andrea';
+let name2='Aigest';
+console.log(`\n Do we have the same names : ${name1===name2}`);
+
+
+let isMember=false;
+let hasCupon=false;
+if (isMember || hasCupon){
+console.log(`\n Member is eligibile for free shiping or not`);
+}
+
+let number=3
+if(number!=0) {
+    console.log(`\n This number is not equal to 0`);
+}
+
+
+let aigestAge=30;
+let oldEnough=18;
+let result=(aigestAge>oldEnough) ?'You are old enough to vote':' You are not old enough to vote';
+console.log(`\n ${result}`);
+
+let price=50;
+let salePrice=70;
+let result2=(price>salePrice)?'This product is on sale':'This product is not on sale';
+console.log(`\n ${result2}`);
+
+
+
+
+
+
+
+
+
+
+
