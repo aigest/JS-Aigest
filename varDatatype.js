08c4924ec671aa5d44281bc612aff40776ac2b90
aigest <134978327+aigest@users.noreply.github.com>
2023-06-26 17:56:20 -0400

    Add files via upload

---
+++ varDatatype.js
@@ -0,0 +1,22 @@
+let myName = 'Aigest'
+console.log(`\nMy name is ${myName}`);
+
+let myAge = '30'
+console.log(`\nI am ${myAge} years old`);
+
+let favoriteColor = ["Black", 'Yellow', 'White'];
+console.log(`\nMy favorite colors are ${favoriteColor}`);
+
+
+console.log(`My name is ${myName} 
+My age is ${myAge} 
+My favorite color ${favoriteColor}`);
+
+
+const isStudent = [30]
+if (myAge <= isStudent) {
+    console.log(true)   
+} 
+else {
+    console.log(false);
+}
